2d044f90ff89da851cc7f28432cf70aa324ee89a
Prashant Sani <contact@prashantsani.com>
2020-03-13 18:59:01 +0530

    fist cut of programing2

--- word-ladder.js
+++ word-ladder.js
@@ -4,10 +4,89 @@
 * @param {string[]} wordList
 * @return {string[][]}
 */
+
+
 var findLadders = function(beginWord, endWord, wordList) {
-  if(beginWord ==='' || beginWord===undefined || beginWord===null || beginWord===[]){
-    return 'begin word is required';
+  if( beginWord ==='' || beginWord===undefined || beginWord===null || beginWord===[] ||
+      endWord ==='' || endWord===undefined || endWord===null || endWord===[]
+    ){
+    return 'beginWord and endWord is required';
   }
+
+  if(typeof(beginWord)!=='string' || typeof(endWord)!=='string'){
+    return 'beginWord and endWord must be of type string';
+  }
+
+  if(beginWord == endWord){
+    return 'beginWord and endWord are same';
+  }
+
+  if(beginWord.length !== endWord.length){
+    return 'beginWord and endWord must have same lengths';
+  }
+
+  if(!Array.isArray(wordList) || wordList.length===0){
+    return 'incorrect wordList';
+  }
+
+  // https://oj.leetcode.com/problems/word-ladder/
+  function replaceChar(str, index, character) {
+    return str.substr(0, index) + character + str.substr(index + 1);
+  }
+
+  function ladderLength(start, end, dict) {
+    var dictTable = {};
+    var endTable = {};
+
+    for (var j = 0; j < end.length; j++) {
+      endTable[replaceChar(end, j, '?')] = i;
+    }
+
+    for (var i = 0; i < dict.length; i++) {
+      for (var j = 0; j < dict[i].length; j++) {
+        dictTable[replaceChar(dict[i], j, '?')] = i;
+      }
+    }
+
+    function solve(str, solutions) {
+      var min = Infinity;
+      var minSolution = null;
+
+      for (var j = 0; j < str.length; j++) {
+        var strKey = replaceChar(str, j, '?');
+
+        if (strKey in endTable) {
+          
+          solutions.push(end);
+          return solutions;
+          
+        } else if (strKey in dictTable && solutions.indexOf(dict[dictTable[strKey]]) === -1) {
+          console.log(solutionsCopy)
+          var solutionsCopy = solutions.slice();
+          solutionsCopy.push(dict[dictTable[strKey]]);
+          var parts = solve(dict[dictTable[strKey]], solutionsCopy);
+          
+          if (parts !== null && parts.length < min) {
+            min = parts.length;
+            minSolution = parts;
+            
+          }
+          
+        }
+      }
+      return minSolution;
+    }
+
+
+    var parts = solve(start, [start]);
+    
+    return parts;
+  }
+
+  return ladderLength(beginWord,endWord,wordList);
+
 }
+
+
 
 module.exports = findLadders;
--- word-ladder.test.js
+++ word-ladder.test.js
@@ -1,5 +1,30 @@
 const findLadders = require('./word-ladder.js');
 
 test('check code with zero ', () => {
-  expect(findLadders()).toBe('begin word is required');
+  expect(findLadders()).toBe('beginWord and endWord is required');
 });
+
+test('testing if beginWord and endWord are strings', () => {
+  expect(findLadders([],{},[])).toBe('beginWord and endWord must be of type string');
+});
+
+test('check when beginWord and endWord are same', () => {
+  expect(findLadders('cat','cat')).toBe('beginWord and endWord are same');
+});
+
+test('when beginWord and endWord have differnt lengths', () => {
+  expect(findLadders('cat','disney',[])).toBe('beginWord and endWord must have same lengths');
+});
+
+test('incorrect word list ', () => {
+  expect(findLadders('cat','dog',[])).toBe('incorrect wordList');
+});
+
+test('First example  ', () => {
+  expect(findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"])).toEqual(  
+    expect.arrayContaining(["hit", "hot", "lot", "log", "cog"])
+  )
+});
+
+
+
